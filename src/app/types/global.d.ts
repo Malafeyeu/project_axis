adc9e45230c06770cfc7543e9bd830c7f421f761
MOLOFEEVilya <m_molo@mail.ru>
2025-04-20 16:02:07 +0300

    add bable, add extract plugin

---
+++ src/app/types/global.d.ts
@@ -0,0 +1,15 @@
+declare module "*.scss" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.png";
+declare module "*.jpeg";
+declare module '*.jpg';
+declare module "*.svg" {
+  import React = require("react");
+  const SVG: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default SVG;
+}
+
+declare const __IS_DEV__: boolean;
