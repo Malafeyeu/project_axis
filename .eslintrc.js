b77958fcef8525819e4e9d00d0da701f02f8938a
MOLOFEEVilya <m_molo@mail.ru>
2025-04-21 17:13:03 +0300

    add stylelint, i18 plugin for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+const { mark } = require("regenerator-runtime");
+
 module.exports = {
   env: {
     browser: true,
@@ -6,6 +8,7 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'airbnb',
+    'plugin:i18next/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -18,6 +21,7 @@ module.exports = {
   plugins: [
     'react',
     '@typescript-eslint',
+    'i18next',
   ],
   rules: {
     'react/jsx-indent': [2, 2],
@@ -37,6 +41,7 @@ module.exports = {
     'import/extensions': 'off',
     'import/no-extraneous-dependencies': 'off',
     'no-underscore-dangle': 'off',
+    'i18next/no-literal-string': ['error', { markupOnly: true }],
   },
   globals: {
     __IS_DEV__: true,
